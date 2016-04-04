ca0409eeaa8c98217103fa1c1fc8c0e0b2ceb843
sergeimaslov86 <sergei.maslov86@gmail.com>
2016-04-04 17:14:31 +0300

    new documentation

--- sections/template_installation/en/shopify_installation_1.php
+++ sections/template_installation/en/shopify_installation_1.php
@@ -1,3 +1,3 @@
 <h3>Logging into account</h3>
-<p>Register your account at <a href="//www.shopify.com/?ref=templatemonster1">shopify.com</a> (if you are already registered, log into your
+<p>Register your account at <a href="//www.shopify.com/">shopify.com</a> (if you are already registered, log into your
     account by adding /admin to your domain name in your browser address bar and entering your email and password).</p>
--- sections/theme_config/ru/editor.php
+++ sections/theme_config/ru/editor.php
@@ -1,6 +1,15 @@
 <h3>Редактор файлов (Template Editor)</h3>
-
-<p>При клике на эту опцию откроется онлайн-редактор файлов шаблона. Вы можете по своему усмотрению редактировать файлы
-    разметки (файлы с расширением <b>.liquid</b>), скрипты (<b>javascript</b>), стили (<b>css</b>) и файл настроек (<b>settings.html</b>).
+<ul class="index-list">
+	<li>На странице настройки шаблона кликните ссылку <strong>Theme options</strong>.
+	<li>Выберите пункт <strong>Edit languages</strong> в выпадающем списке.</li>
+</li>
+</ul>
+		<figure class="img-polaroid">
+			<img alt="" src="img/09_code_editor.png">
+		</figure>
+<p>Перед вами онлайн-редактор файлов шаблона. Вы можете по своему усмотрению редактировать файлы разметки 
+(файлы с расширением <strong>.liquid</strong>), скрипты <strong>(javascript)</strong>, стили (scss) и файл настроек
+ <strong>(settings_schema.json)</strong>.
 </p>
-<figure class="img-polaroid"><img alt="" src="img/07_editor.png"></figure>
+
+
