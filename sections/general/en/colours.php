1e9073b339999787bb443aa768ca5a59724442d4
LinguistMB <MariaBerkova.MB@gmail.com>
2017-03-30 10:44:50 +0300

    Update colours.php

--- sections/general/en/colours.php
+++ sections/general/en/colours.php
@@ -10,5 +10,5 @@
 
   <p>In this section, you choose the colors for your site. The   section includes two subsections: <strong>Page colors</strong> and <strong>Color theme</strong>.</p>
 <p>Let's explore the options:</p>
-  <p>The<strong> Page colors </strong>option controls your<strong>Page background color</strong> and <strong>Border color</strong>.</p>
+  <p>The<strong> Page colors </strong>option controls the <strong>Page background color</strong> and <strong>Border color</strong>.</p>
   <p align="justify">The<strong> Color theme</strong> is used for alternate button styles, some text and icon hover states, and other places.  In this section you can adjust  links, top bar, header, navigation and footer colors.</p>
