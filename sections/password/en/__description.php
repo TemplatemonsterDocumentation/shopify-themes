d69c484b2f7d2e2236470198f8d124b3fe8573fd
LinguistMB <Linguist@templatemonster.me>
2017-12-04 15:02:48 +0200

    add new v2-1

---
+++ sections/password/en/__description.php
@@ -0,0 +1,227 @@
+<h2 class="item4">Password Page  Configuration</h2>
+
+<p>Now you can  display the &quot;Site under maintenance&quot; page and let your  customers know that now there is some technical work going on and  that the website will soon become available. <br />
+With the help of  this extension, you can change the appearance of this page. You can  specify the end time of the service work and display a timer to let  your clients know the timing of this work. The module allows you  display a subscription form for this page, and in the end of the work  you can send e-mail notifications to your clients.</p>
+<p>Moreover, you can protect your online store with the password.</p>
+<p>
+  <figure class="img-polaroid">
+    <img alt="" src="img/password/pass-3.png">
+  </figure>
+</p>
+
+  <h6><strong>Content</strong>:</h6>
+  <ul>
+    <li><a href="#1" align="justify">1. How to enable the Password Page</a></li>
+    <li><a href="#2" align="justify">2. How to customize the Password Page</a></li>
+  
+    <li><a href="#3" align="justify">3. Upload the Logo</a></li>
+    <li><a href="#4" align="justify">4. Add the page header</a></li>
+    <li><a href="#5" align="justify">5. Add the text message</a></li>
+    <li><a href="#6" align="justify">6. Change the background image</a></li>
+    <li><a href="#7" align="justify">7. Change the password label</a></li>
+     <li><a href="#8" align="justify">8. Enable the newsletter form </a></li>
+      <li><a href="#9" align="justify">9. Set the countdown timer</a></li>
+       <li><a href="#10" align="justify">10. Enable the Social links block</a></li>
+        <li><a href="#11" align="justify">11. Set the password</a></li>
+             <li><a href="#12" align="justify">12. Add the Message for your visitors</a></li>
+                  <li><a href="#13" align="justify">13. How to disable the Password Page</a></li>
+  </ul>
+
+
+  
+  
+  <a name="1">  </a><h5><strong>How to enable the Password Page</strong></h5>
+<p>Navigate to the <strong>Online Store &gt; </strong> <strong>Preferences</strong> section. </p>
+<p>
+  <figure class="img-polaroid">
+    <img alt="" src="img/password/pref.png">
+  </figure>
+</p>
+<p>Go to the<strong> Password page</strong> section.</p>
+<p>Tick the <strong>Enable password page</strong> checkbox.</p><p>
+  <figure class="img-polaroid">
+    <img alt="" src="img/password/pass.png">
+  </figure>
+</p
+
+
+><p>Note, you can change the password here and add the additional information to show  on the Password Page.
+</p>
+<p><strong>Save</strong> the settings.</p>
+<p>
+  <figure class="img-polaroid">
+    <img alt="" src="img/password/save.png">
+  </figure>
+</p>
+<a name="2">  </a>
+<h5><strong>How to customize the Password Page</strong></h5>
+<p>Navigate to the <strong>Online Store &gt; </strong> <strong>Themes</strong> section. </p>
+
+<p>
+  <figure class="img-polaroid">
+    <img alt="" src="img/password/theme.png">
+  </figure>
+</p
+><p>Select the theme you want to add the  Password Page to.</p>
+<p>Click on the <strong>Customize theme</strong> button to open the  theme editor page.</p>
+<p>
+  <figure class="img-polaroid">
+    <img alt="" src="img/password/cust.png">
+  </figure>
+</p
+><p>Go to the<strong> General </strong>tab. Then click <strong>Layouts</strong>.</p>
+<p>
+  <figure class="img-polaroid">
+    <img alt="" src="img/password/layouts.png">
+  </figure>
+</p
+>
+<p>Tick a <strong>Show a link to the password page, to customize the display  of the page </strong>checkbox.</p>
+<p>
+  <figure class="img-polaroid">
+    <img alt="" src="img/password/show.png">
+  </figure>
+
+<p>Remember to SAVE the settings.</p>
+<p>Press on the VIEW PASSWORD PAGE button to start the page  customization.</p>
+<figure class="img-polaroid"><img alt="" src="img/password/button.png"></figure>
+<h4><strong>Sections Tab </strong></h4>
+<p>Go to the<strong> Sections</strong> section, double-click on <strong>Password</strong> tab to open the   page settings.</p>
+<figure class="img-polaroid"><img alt="" src="img/password/set-20.png"></figure>
+<a name="3">  </a>
+<h5><strong>Upload the Logo</strong></h5>
+<p>In this section you can set the password page Logo. </p>
+<figure class="img-polaroid"><img alt="" src="img/password/set-1.png"></figure>
+
+<p>The Logo will be scaled to 90px height.</p>
+<p>You are able to Upload desired image from your computer or select image from your library. </p>
+<figure class="img-polaroid"><img alt="" src="img/password/set-2.png"></figure>
+
+<p>You are able to edit an image. Select<strong> Edit image</strong> from the <strong>Update</strong> dropdown.</p>
+<p>Write a brief description of this image to improve search engine optimization (SEO) and accessibility for visually impaired customers.</p>
+<figure class="img-polaroid"><img alt="" src="img/password/set-3.png"></figure>
+
+<p>Remember to SAVE the settings</p>
+<p>You can delete Logo image. In order to do this, press the<strong> Remove</strong> button.</p>
+<p>
+  <figure class="img-polaroid"><img alt="" src="img/password/set-4.png"></figure>
+  
+</p>
+<a name="4">  </a>
+<h5><strong>Add the page header</strong></h5>
+<p>You can change the page header. Enter the text you want to display as the header in the <strong>Page header</strong> option textfield.</p>
+<p>
+  <figure class="img-polaroid"><img alt="" src="img/password/set-5.png"></figure>
+  
+</p>
+<p>Remember to SAVE the settings.</p>
+<a name="5">  </a>
+<h5><strong>Add the text message</strong></h5>
+<p>You can change the text message. Enter the text you want to display as the text message (under the page header) in the <strong>Text message </strong>option textfield.</p>
+<p>
+  <figure class="img-polaroid"><img alt="" src="img/password/set-6.png" /></figure>
+</p>
+<p>Remember to SAVE the settings.</p>
+<a name="6">  </a>
+<h5><strong>Change the background image</strong> </h5>
+<p>You can change the background image for the password page.</p>
+<p>
+  <figure class="img-polaroid"><img alt="" src="img/password/set-7.png" /></figure>
+</p>
+<p>You are able to Upload desired image from your computer or select image from your library. For higher quality upload images wider than 2048 pixels.</p>
+<p>You are able to edit an image. Select<strong> Edit image</strong> from the <strong>Update</strong> dropdown.</p>
+<p>Remember to SAVE the settings</p>
+<p>You can delete the background image. In order to do this, press the<strong> Remove</strong> button.</p>
+<a name="7">  </a>
+<h5><strong>Change the password label</strong> </h5>
+<p>You can change the password label. Enter the text you want to display as the password label (above the password form) in the <strong>Password label </strong>option textfield.</p>
+<p>
+  <figure class="img-polaroid"><img alt="" src="img/password/set-8.png" /></figure>
+</p>
+<p>Remember to SAVE the settings.</p>
+<a name="8">  </a>
+<h5><strong>Enable the newsletter form </strong></h5>
+<p>To enable the newsletter form for the password page tick the <strong>Show newsletter form </strong>checkbox.</p>
+<p>
+  <figure class="img-polaroid"><img alt="" src="img/password/set-9.png" /></figure>
+</p>
+<p>Remember to SAVE the settings.</p>
+<a name="9">  </a>
+<h5><strong>Set the countdown timer</strong></h5>
+<p>To enable the countdown timer  for the password page tick the <strong>Show countdown timer </strong>checkbox.</p>
+<p>
+  <figure class="img-polaroid"><img alt="" src="img/password/set-10.png" /></figure>
+</p>
+
+<p>You can set the countdown timer. Choose the date when site will be online from the<strong> Day</strong>, <strong>Month</strong> and <strong>Year</strong> dropdowns.</p>
+<p>
+  <figure class="img-polaroid"><img alt="" src="img/password/set-11.png" /></figure>
+</p>
+<p>Remember to SAVE the settings.</p>
+<a name="10">  </a>
+<h5><strong>Enable the Social links block</strong></h5>
+<p>To enable the social links block for the password page tick the <strong>Show social menu </strong>checkbox.</p>
+<p>
+  <figure class="img-polaroid"><img alt="" src="img/password/set-12.png" /></figure>
+</p>
+
+
+<p>In order to show the social share button, enter the link into the social media option textfield.</p>
+<p>
+  <figure class="img-polaroid"><img alt="" src="img/password/set-15.png" /></figure>
+</p>
+<p>If the textfield is blank, the social media button will be disabled by default.</p>
+<figure class="img-polaroid"><img alt="" src="img/password/set-16.png" /></figure>
+<a name="11">  </a>
+<h5><strong>Set the password</strong></h5>
+<p>Navigate to the <strong>Online Store &gt; </strong> <strong>Preferences</strong> section. </p>
+<p>
+  <figure class="img-polaroid"> <img alt="" src="img/password/pref.png" /> </figure>
+</p>
+<p>Go to the<strong> Password page</strong> section.</p>
+<p>
+  <figure class="img-polaroid"> <img alt="" src="img/password/set-18.png" /> </figure>
+</p
+
+
+>
+<p>Enter the password.</p>
+<p><strong>Save</strong> the settings.</p>
+<a name="12">  </a>
+<h5><strong>Add the Message for your visitors</strong></h5>
+<p>Navigate to the <strong>Online Store &gt; </strong> <strong>Preferences</strong> section. </p>
+<p>
+  <figure class="img-polaroid"> <img alt="" src="img/password/pref.png" /></figure>
+</p>
+<p>Go to the<strong> Password page</strong> section.</p>
+<p>
+  <figure class="img-polaroid"> <img alt="" src="img/password/set-18.png" /></figure>
+</p
+
+
+>
+<p>Enter the message.</p>
+<p>
+  <figure class="img-polaroid"> <img alt="" src="img/password/set-19.png" /></figure>
+</p> <p><strong>Save</strong> the settings.</p>
+<a name="13">  </a>
+<h5><strong>How to disable the Password Page</strong></h5>
+<p>Navigate to the <strong>Online Store &gt; </strong> <strong>Preferences</strong> section. </p>
+<p>
+  <figure class="img-polaroid"> <img alt="" src="img/password/pref.png" /> </figure>
+</p>
+<p>Go to the<strong> Password page</strong> section.</p>
+<p>Disable the checkbox for <strong>Enable password page</strong>.</p>
+<p>
+  <figure class="img-polaroid"> <img alt="" src="img/password/pass-1.png" /> </figure>
+</p
+
+
+>
+<p><strong>Save</strong> the settings.</p>
+<p>
+  <figure class="img-polaroid"> <img alt="" src="img/password/save.png" /> </figure>
+</p
+
+
+>
---
+++ sections/sections/en/instagram.php
@@ -0,0 +1,25 @@
+<h3>Instagram Section
+</h3>
+<p>
+  <figure class="img-polaroid">
+    <img alt="" src="img/instagram.jpg">
+  </figure>
+  
+</p>
+<h5>Let's review the Instagram section
+configuration:</h5>
+<p>
+  <figure class="img-polaroid">
+    <img alt="" src="img/instagram-1.jpg">
+  </figure>
+  
+</p>
+<ol class="marked-list">
+  <li><strong>Heading</strong> — add the title of the section;
+  </li>
+<li><strong>Instagram user ID</strong> — specify the Instagram user ID. You can get  user ID <a href="https://smashballoon.com/instagram-feed/find-instagram-user-id/">here</a>; </li>
+<li><strong>Instagram access token </strong> — enter your access token. Find out how to <a href="https://elfsight.com/blog/2016/05/how-to-get-instagram-access-token/">receive Instagram Access Token</a>;</li>
+<li><strong>Number of images</strong> —  	set a number of article to show. Note, the photos will autoupdate automatically. (6/12).</li>
+</ol>
+<p>Remember to <strong>Save</strong> the settings.</p>
+<p>To remove the section press  the <strong>Delete Section</strong> button.</p>
