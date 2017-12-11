e8d9ee7891c75fcb9429459538e39e0c44ad8543
LinguistMB <MariaBerkova.MB@gmail.com>
2017-12-11 15:50:02 +0200

    Add files via upload

---
+++ sections/general/ru/product-badge.php
@@ -0,0 +1,27 @@
+<h3>Products Badge</h3>
+<p>Add  &quot;New&quot; and &quot;Sale&quot; badges  to product images for the collection page. </p>
+<p>
+  <figure class="img-polaroid">
+    <img alt="" src="img/product-badge.jpg">
+  </figure>
+  
+</p>
+<h5>Let's review the options available:</h5>
+<ol class="marked-list">
+  <li>To show the labels, you should first enable the <strong>Show badge on product </strong>option.</li>
+  <figure class="img-polaroid">
+    <img alt="" src="img/product-badge-2.jpg">
+  </figure>
+  <li><strong>'New' products marker</strong> — enter the number of days the product is considered as &quot;New&quot; (the time period starts from the date the product was added). </li>
+
+<figure class="img-polaroid">
+    <img alt="" src="img/product-badge-1.jpg">
+  </figure>
+  <li><strong>'Sale' marker type</strong> — set the &quot;Sale&quot; label type (Text or Percent). The &quot;Sale&quot; label appears automaticaly after the <strong>Compare at price</strong> is set under the <strong>Products</strong> &gt; <strong>All products</strong> section, in case the <strong>Show badge on product </strong>option  is activated. </li>
+
+<figure class="img-polaroid">
+    <img alt="" src="img/product-badge-3.jpg">
+  </figure>
+
+</ol>
+<p>Remember to <strong>Save</strong> the settings.</p>
---
+++ sections/general/ru/sidebar.php
@@ -0,0 +1,176 @@
+<h3>Sidebar
+
+
+</h3>
+<h4>General settings</h4>
+<ol class="index-list">
+  <li>Enable the <strong>Show sidebar</strong> option to display sidebar</li>
+  <li>Choose the sidebar position. </li>
+  <li>Remember to <strong>Save</strong> the settings.</li>
+</ol>
+<figure class="img-polaroid">
+    <img alt="" src="img/sidebar.jpg">
+</figure>
+<h4>Sidebar widgets for the Collection and Blog page</h4>
+<p>Move to the <strong>Sections</strong> tab and click on the <strong>Sidebar</strong> tab.</p>
+<figure class="img-polaroid">
+    <p><img alt="" src="img/sidebar-1.jpg">
+    </p></figure>
+  <div>
+                                <h6><strong>Content</strong>:</h6>
+<a href="#collections">  <p align="justify">1. Collections section</p></a>
+  <a href="#product-types">  <p align="justify">2. Product types section</p></a>
+                               <a href="#vendors"> <p align="justify">3. Product vendors section</p>
+                               </a>
+                               <a href="#products"> <p align="justify">3. Products section</p>
+                               </a>
+                               <a href="#linklist"> <p align="justify">4. Linklist section</p>
+                               </a>
+                               <a href="#html"> <p align="justify">5. Custom HTML section</p>
+                               </a>
+                               <a href="#banner"> <p align="justify">6. Banner section</p>
+                               </a></div>
+
+<a name="collections"></a>
+<h5><strong>Add Collections section</strong></h5>
+<figure class="img-polaroid">
+      <p><img alt="" src="img/sidebar-10.jpg" /> </p>
+</figure>
+<ol class="index-list">
+  <li>To add the Collections section to the sidebar, press on the <strong>Add content</strong> and select <strong>Collections</strong> from the dropdown.</li>
+
+<figure class="img-polaroid">
+    <img alt="" src="img/sidebar-3.jpg">
+</figure>
+
+  <li>The new tab will appear in the <strong>Content</strong> section.</li>
+  <li>Click on the <strong>Collections</strong> tab to configure the section. </li>
+
+<figure class="img-polaroid">
+    <img alt="" src="img/sidebar-4.jpg">
+</figure>
+
+  <li>Enter the <strong>title</strong> for the block. All in-store categories will be displayed in this section. You can add new <strong>Collections </strong>under the <strong>Product</strong>s &gt; <strong>Collections</strong> menu.</li>
+  <li>You can delete the section as well. Click <strong>Delete</strong> in order to remove the section.</li>
+</ol>
+<figure class="img-polaroid">
+    <p><img alt="" src="img/sidebar-5.jpg">
+</p></figure>
+<a name="product-types"></a>
+
+<h5><strong>Product types section</strong></h5>
+<figure class="img-polaroid">
+      <p><img alt="" src="img/sidebar-11.jpg" /> </p>
+</figure>
+<ol class="index-list">
+  <li>To add the Product types section to the sidebar, press on the <strong>Add content</strong> and select <strong>Product types</strong> from the dropdown.</li>
+
+<figure class="img-polaroid"> <img alt="" src="img/sidebar-6.jpg" /> </figure>
+
+  <li>The new tab will appear in the <strong>Content</strong> section.</li>
+  <li>Click on the <strong>Product types </strong>tab to configure the section. </li>
+
+<figure class="img-polaroid">
+  <p><img src="img/sidebar-9.jpg" alt="" height="" /> </p>
+</figure>
+   
+  
+      <li>Enter the <strong>title</strong> for the block. </li>
+      <li> Click <strong>Delete</strong> to remove the section.</li>
+</ol>
+<a name="vendors"></a>
+
+<figure class="img-polaroid">
+<p><img src="img/sidebar-12.jpg" alt="" height="" /> </p></figure>
+      <h5><strong>Product vendors section</strong></h5>
+<figure class="img-polaroid">
+        <p><img alt="" src="img/sidebar-14.jpg" /></p>
+</figure>
+      <ol class="index-list">
+        <li>To add the Product vendors section to the sidebar, press on the <strong>Add content</strong> and select <strong>Product vendors</strong> from the dropdown.</li>
+      
+<figure class="img-polaroid"> <img alt="" src="img/sidebar-15.jpg" /></figure>
+  
+        <li>The new tab will appear in the <strong>Content</strong> section.</li>
+        <li>Click on the <strong>Product vendors </strong>tab to configure the section. </li>
+        <li>Enter the <strong>title</strong> for the block. </li>
+        <li> Click <strong>Delete</strong> to remove the section.</li>
+      </ol>
+<figure class="img-polaroid">
+<p><img src="img/sidebar-16.jpg" alt="" height="" /></p></figure>
+<a name="products"></a>
+
+        <h5><strong>Products section</strong></h5>
+<figure class="img-polaroid">
+          <p><img alt="" src="img/sidebar-23.jpg" /></p>
+</figure>
+<ol class="index-list">
+          <li>To add the Product  section to the sidebar, press on the <strong>Add content</strong> and select <strong>Products</strong> from the dropdown.</li>
+     
+<figure class="img-polaroid"> <img alt="" src="img/sidebar-18.jpg" /></figure>
+        
+          <li>The new tab will appear in the <strong>Content</strong> section.</li>
+          <li>Click on the <strong>Products </strong>tab to configure the section. </li>
+          <li>Enter the <strong>title</strong> for the block and select the Collection. </li>
+          <li>The last added 3  products from the selected collection will be shown  in the <strong>Products</strong> section.</li>
+          <li> Click <strong>Delete</strong> to remove the section.</li>
+</ol>
+        <figure class="img-polaroid">
+          <p><img src="img/sidebar-21.jpg" alt="" height="" /></p>
+</figure>
+<a name="linklist"></a>
+
+               <h5><strong>Linklist section</strong></h5>
+<figure class="img-polaroid">
+          <p><img alt="" src="img/sidebar-25.jpg" /></p>
+</figure>
+<ol class="index-list">
+          <li>To add the Linklist  section to the sidebar, press on the <strong>Add content</strong> and select<strong> Linklist </strong>from the dropdown.</li>
+     
+<figure class="img-polaroid"> <img alt="" src="img/sidebar-26.jpg" /></figure>
+        
+          <li>The new tab will appear in the <strong>Content</strong> section.</li>
+          <li>Click on the <strong>Linklist </strong>tab to configure the section. </li>
+          <li>Choose the linklist to display. Add new linklist under the <strong>Online store</strong> &gt; <strong>Navigation</strong> menu.</li>
+          <li> Click <strong>Delete</strong> to remove the section.</li>
+</ol>
+        <figure class="img-polaroid">
+          <p><img src="img/sidebar-27.jpg" alt="" height="" /></p></figure>
+          <a name="html"></a>
+
+<strong>          <h5>Custom HTML section</h5></strong>
+
+
+<ol class="index-list">
+          <li>To add the block with custom HTML to the sidebar, press on the <strong>Add content</strong> and select<strong> HTML code </strong>from the dropdown.</li>
+     
+<figure class="img-polaroid"> <img alt="" src="img/sidebar-28.jpg" /></figure>
+        
+          <li>The new tab will appear in the <strong>Content</strong> section.</li>
+          <li>Click on the<strong> {/} HTML code </strong>tab to configure the section. </li>
+          <li>Enter the code in the <strong>Custom HTML</strong> option field.</li>
+          <li> Click <strong>Delete</strong> to remove the section.</li>
+</ol>
+        <figure class="img-polaroid">
+          <p><img src="img/sidebar-29.jpg" alt="" height="" /></p>
+          
+        </figure>
+       <a name="banner"></a>
+
+      <h5><strong>Add Banner  section</strong></h5>
+<figure class="img-polaroid"> <img alt="" src="img/sidebar-31.jpg" /></figure>
+        <ol class="index-list">
+          <li>To add the Banner to the sidebar, press on the <strong>Add content</strong> and select <strong>Banner </strong>from the dropdown.</li>
+          <figure class="img-polaroid"> <img alt="" src="img/sidebar-28.jpg" /></figure>
+          <li>The new tab will appear in the <strong>Content</strong> section.</li>
+          <li>Click on the<strong> Banner (Title)</strong> tab to configure the section. </li>
+          <li>To chose the image for the banner, press the <strong>Select image</strong> button. To upload image, go to the<strong> Settings</strong> &gt; <strong>Files</strong> menu and click <strong>Upload files</strong> button. Note, the image should be more than <strong>270 px </strong>wide.</li>
+          <li>Set the banner height (Small/Medium/Large).</li>
+          <li>Specify the <strong>Heading</strong> for the banner and add some<strong> Text</strong> you want yo display on the banner.</li>
+          <li>You also can attach the <strong>link</strong> to the banner.</li>
+          <li> Click <strong>Delete</strong> to remove the section.</li>
+        </ol>
+        <figure class="img-polaroid">
+          <p><img src="img/sidebar-32.jpg" alt="" height="" /></p>
+</figure>
+   
